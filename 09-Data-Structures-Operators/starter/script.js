10b338ccd8ee93ea9cf2666320f09f5ba74b88f0
HBhawe <harshalb97@outlook.com>
2024-09-11 17:08:38 +0200

    For Of loops

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -338,31 +338,40 @@ const game = {
 };
 
 // const players1 = [...game.players[0]];
-const [players1, players2] = game.players;
 // const players2 = [...game.players[1]];
-console.log(players1, players2);
 
-const [gk, ...fieldPlayers] = players1;
-console.log(gk, fieldPlayers);
+// const [players1, players2] = game.players;
 
-const allPlayers = [...players1, ...players2];
-console.log(allPlayers);
+// console.log(players1, players2);
 
-const players1Final = [...players1, "Thiago", "Coutinho", "Perisic"];
+// const [gk, ...fieldPlayers] = players1;
+// console.log(gk, fieldPlayers);
 
-const { team1, x: draw, team2 } = game.odds;
-console.log(team1, draw, team2);
+// const allPlayers = [...players1, ...players2];
+// console.log(allPlayers);
 
-const printGoals = function (...players) {
-  console.log(`${players.length} goals were scored`);
-};
+// const players1Final = [...players1, "Thiago", "Coutinho", "Perisic"];
 
-printGoals("Davies", "Muller", "Lewandowski", "Kimmich");
-printGoals("Davies", "Muller");
-printGoals(...game.scored);
+// const { team1, x: draw, team2 } = game.odds;
+// console.log(team1, draw, team2);
+
+// const printGoals = function (...players) {
+//   console.log(`${players.length} goals were scored`);
+// };
+
+// printGoals("Davies", "Muller", "Lewandowski", "Kimmich");
+// printGoals("Davies", "Muller");
+// printGoals(...game.scored);
 
 // team1 < team2
 //   ? console.log("Team 1 is more likely to win")
 //   : console.log("Team 2 is more likely to win");
 
-team1 < team2 && console.log("Team 1 is more likely to win");
+// team1 < team2 && console.log("Team 1 is more likely to win");
+
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+// FOR OF loop
+for (const item of menu) {
+  console.log(item);
+}
