a2066a5102bbf113e7783ffe2f45a9690a8e5e0d
HBhawe <harshalb97@outlook.com>
2024-09-12 16:51:50 +0200

    Updated for lesson 115

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -4,6 +4,21 @@
 const flights =
   "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
 
+const openingHours = {
+  thu: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 // Data needed for first part of the section
 const restaurant = {
   name: "Classico Italiano",
@@ -16,20 +31,7 @@ const restaurant = {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
+  openingHours,
 
   orderDelivery: function ({
     starterIndex = 1,
@@ -296,46 +298,46 @@ const restaurant = {
 
 // console.log(rest1, rest2);
 
-const game = {
-  team1: "Bayern Munich",
-  team2: "Borrussia Dortmund",
-  players: [
-    [
-      "Neuer",
-      "Pavard",
-      "Martinez",
-      "Alaba",
-      "Davies",
-      "Kimmich",
-      "Goretzka",
-      "Coman",
-      "Muller",
-      "Gnarby",
-      "Lewandowski",
-    ],
-    [
-      "Burki",
-      "Schulz",
-      "Hummels",
-      "Akanji",
-      "Hakimi",
-      "Weigl",
-      "Witsel",
-      "Hazard",
-      "Brandt",
-      "Sancho",
-      "Gotze",
-    ],
-  ],
-  score: "4:0",
-  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
-  date: "Nov 9th, 2037",
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
+// const game = {
+//   team1: "Bayern Munich",
+//   team2: "Borrussia Dortmund",
+//   players: [
+//     [
+//       "Neuer",
+//       "Pavard",
+//       "Martinez",
+//       "Alaba",
+//       "Davies",
+//       "Kimmich",
+//       "Goretzka",
+//       "Coman",
+//       "Muller",
+//       "Gnarby",
+//       "Lewandowski",
+//     ],
+//     [
+//       "Burki",
+//       "Schulz",
+//       "Hummels",
+//       "Akanji",
+//       "Hakimi",
+//       "Weigl",
+//       "Witsel",
+//       "Hazard",
+//       "Brandt",
+//       "Sancho",
+//       "Gotze",
+//     ],
+//   ],
+//   score: "4:0",
+//   scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+//   date: "Nov 9th, 2037",
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
 
 // const players1 = [...game.players[0]];
 // const players2 = [...game.players[1]];
@@ -369,9 +371,61 @@ const game = {
 
 // team1 < team2 && console.log("Team 1 is more likely to win");
 
-const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
 // FOR OF loop
-for (const item of menu) {
-  console.log(item);
+// for (const item of menu) {
+//   console.log(item);
+// }
+
+// getting the index out with an array iterator
+// for (const [i, el] of menu.entries()) {
+//   console.log(`${i + 1}: ${el}`);
+// }
+
+// OBJECT LITERALS
+// refer opening hours example at the top
+// the function keyword can be removed inside the object and replaced with function name and parameters
+
+// const weekdays = ["mon", "tue", "wed", "thurs", "fri", "sat", "sun"];
+
+// OPTIONAL CHAINING
+// console.log(restaurant.openingHours.mon.open);
+
+// only if monday exists will the open property be read
+// console.log(restaurant.openingHours.mon?.open);
+
+// const days = ["mon", "tue", "wed", "thurs", "fri", "sat", "sun"];
+
+// for (const day of days) {
+//   // console.log(day);
+//   const open = restaurant.openingHours[day]?.open ?? "closed";
+//   console.log(`on ${day} we open at ${open}`);
+// }
+
+// METHODS
+// console.log(restaurant.order?.(0, 1) ?? `method does not exist`);
+
+// console.log(restaurant.orderRisotto?.(0, 1) ?? `method does not exist`);
+
+// const users = [
+//   {
+//     name: "Harshal",
+//     email: "hello@harshal.com",
+//   },
+// ];
+
+// console.log(users[0]?.name ?? `user array empty`);
+// console.log(users[1]?.name ?? `user array empty`);
+
+for (const day of Object.keys(openingHours)) {
+  console.log(day);
+}
+
+// entire object
+const entries = Object.entries(openingHours);
+console.log(entries);
+
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key} we open at ${open} and close at ${close}`);
 }
