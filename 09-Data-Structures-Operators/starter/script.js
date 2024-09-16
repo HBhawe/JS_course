8f36f3fd9a61ad116f889d2fb19f8d46fbc85a8f
HBhawe <harshalb97@outlook.com>
2024-09-16 14:08:45 +0200

    Coding challenge 3

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -559,3 +559,39 @@ const game = {
 // console.log([...question]);
 // console.log([...question.entries()]);
 // console.log([...question.values()]);
+
+// CODING CHALLENGE 3
+const gameEvents = new Map([
+  [17, "⚽ GOAL"],
+  [36, "🔁 Substitution"],
+  [47, "⚽ GOAL"],
+  [61, "🔁 Substitution"],
+  [64, "🔶 Yellow card"],
+  [69, "🔴 Red card"],
+  [70, "🔁 Substitution"],
+  [72, "🔁 Substitution"],
+  [76, "⚽ GOAL"],
+  [80, "⚽ GOAL"],
+  [92, "🔶 Yellow card"],
+]);
+
+// 1.
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+// 2.
+console.log(gameEvents);
+// gameEvents.delete(64);
+// console.log(gameEvents);
+
+// 3.
+// console.log(gameEvents.size);
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+
+// 4.
+for (const [minute, event] of gameEvents) {
+  const half = minute <= 45 ? "FIRST" : "SECOND";
+  console.log(`[${half} HALF] ${minute}: ${event}`);
+}
