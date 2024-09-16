4fbdb58727949d84295f44f875ed6bb641c757da
HBhawe <harshalb97@outlook.com>
2024-09-16 13:26:52 +0200

    Maps

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -492,3 +492,70 @@ const game = {
 // );
 
 // MAPS
+// key-value pairs - similar to objects
+// any data type can be a key
+
+// const rest = new Map();
+// rest.set("name", "classico Italiano");
+// rest.set(1, "Firenze, Italy");
+// rest.set(2, "Lisbon, Portugal");
+// console.log(rest);
+
+// rest
+//   .set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"])
+//   .set("open", 11)
+//   .set("close", 23)
+//   .set(true, "we are open :D")
+//   .set(false, "We are close :(");
+
+// console.log(rest);
+// console.log(rest.get("name"));
+// console.log(rest.get(true));
+// console.log(rest.get(1));
+
+// const time = 21;
+// console.log(rest.get(time > rest.get("open") && time < rest.get("close")));
+
+// console.log(rest.has("categories"));
+// rest.delete(2);
+// rest.clear();
+// console.log(rest);
+// console.log(rest.size);
+
+// rest.set(document.querySelector("h1"), "heading");
+// console.log(rest);
+
+const question = new Map([
+  ["question", "What is the best programming language in the world?"],
+  [1, "C"],
+  [2, "Java"],
+  [3, "JavaScript"],
+  ["correct", 3],
+  [true, "Correct"],
+  [false, "Wrong"],
+]);
+
+console.log(question);
+
+// convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// ARRAYS
+console.log(question.get("question"));
+for (const [key, value] of question) {
+  if (typeof key === "number") {
+    console.log(`Answer ${key}: ${value}`);
+  }
+}
+
+// const answer = Number(prompt("Your answer"));
+// console.log(answer);
+
+// console.log(question.get(answer === question.get("correct")));
+
+// MAP BACK TO AN ARRAY
+console.log([...question]);
+console.log([...question.entries()]);
+console.log([...question.values()]);
