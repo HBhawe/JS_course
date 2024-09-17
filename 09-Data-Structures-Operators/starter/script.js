e249e853aab133b9b2a6c450019cea662c5e558d
HBhawe <harshalb97@outlook.com>
2024-09-17 14:49:54 +0200

    Strings challenge

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -757,3 +757,40 @@ const gameEvents = new Map([
 // planesInLine(5);
 // planesInLine(3);
 // planesInLine(12);
+
+document.body.append(document.createElement("textarea"));
+document.body.append(document.createElement("button"));
+const button = document.querySelector("button");
+button.textContent = "click";
+
+const text = document.querySelector("textarea").value;
+
+const camelCase = function () {
+  const text = document.querySelector("textarea").value;
+  const array = text.toLowerCase().split("\n");
+  const camelCaseArray = [];
+  // for (const string of array) {
+  //   const text = string.trim().split("_");
+  //   const camelCaseStr = [
+  //     text[0] + text[1].replace(text[0][1], text[0][1].toUpperCase()),
+  //   ].join("");
+  //   camelCaseArray.push(camelCaseStr);
+  // }
+
+  // for (let index = 0; index < camelCaseArray.length; index++) {
+  //   const element = camelCaseArray[index];
+  //   console.log(`${element.padEnd(20, " ")} ${"✅".repeat(index + 1)}`);
+  // }
+
+  // better way to do it
+  for (const [i, string] of array.entries()) {
+    const [first, second] = string.trim().split("_");
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(`${output.padEnd(20)}${"✅".repeat(i + 1)}`);
+  }
+};
+
+button.addEventListener("click", camelCase);
