40041dde67ef5f2228f58e85ec99faba16d396dd
HBhawe <harshalb97@outlook.com>
2024-09-17 16:56:09 +0200

    Functions 1

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,64 @@
 "use strict";
+
+// const bookings = [];
+
+// const createBooking = function (
+//   flightNum,
+//   numPassengers = 1,
+//   price = 199 * numPassengers
+// ) {
+//   //   ES5
+//   //   numPassengers = numPassengers || 1;
+//   //   price = price || 199;
+
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
+
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+
+// createBooking("LH123");
+// createBooking("LH123", 2, 800);
+// createBooking("LH123", 2);
+// createBooking("LH123", 5);
+
+// // cannot do this - returns error
+// // createBooking("LH123", , 500);
+
+// createBooking("LH123", undefined, 5000);
+
+// const flight = "LH1234";
+// const harshal = {
+//   name: "Harshal Bhawe",
+//   passport: "43534gk",
+// };
+
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = "LH999";
+//   passenger.name = "Mr. " + passenger.name;
+
+//   if (passenger.passport === "43534gk") {
+//     alert("Check in");
+//   } else alert("Wrong passport");
+// };
+
+// // checkIn(flight, harshal);
+// // console.log(flight, harshal);
+
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 100000000);
+// };
+
+// newPassport(harshal);
+// // checkIn(flight, harshal);
+
+// console.log(harshal);
+
+// passing by value and passing by reference
+// there is NO pass by reference in JS
+
+// FIRST CLASS AND HIGHER ORDER FUNCTIONS
