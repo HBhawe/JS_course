4f58ae14971dd08f86dcf04574daee86c4ab4490
HBhawe <harshalb97@outlook.com>
2024-09-26 16:57:03 +0200

    Functions closures a

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -281,15 +281,31 @@
 // runOnce();
 
 // we short circuit JS by wrapping with ()
-(function () {
-  console.log(`This will never run again`);
-})();
-// empty parentheses signify a "function" call
-(() => console.log(`This will ALSO never run again`))();
+// (function () {
+//   console.log(`This will never run again`);
+// })();
+// // empty parentheses signify a "function" call
+// (() => console.log(`This will ALSO never run again`))();
 
-{
-  const isPrivate = 23;
-  var notPrivate = 23;
-}
-// console.log(isPrivate);
-console.log(notPrivate);
+// {
+//   const isPrivate = 23;
+//   var notPrivate = 23;
+// }
+// // console.log(isPrivate);
+// console.log(notPrivate);
+
+// CLOSURES
+
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
+booker();
+booker();
+booker();
