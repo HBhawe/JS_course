ebb4a826d3d7be73afdfba0b149d5368c695ea68
Harshal Bhawe <harshalb97@outlook.com>
2024-09-28 20:50:30 +0200

    Arrays 2

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -118,3 +118,22 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // // JOIN
 // console.log(letters.join(" - "));
+
+// ARRAYS 2
+const arr = [23, 11, 64];
+
+// traditional way of doing this
+console.log(arr[0]);
+
+// NEW IN ES 2022
+console.log(arr.at(0));
+
+// last element - traditional ways
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+
+// NEW IN ES 2022
+console.log(arr.at(-1));
+
+// also works on strings
+console.log("Harshal".at(-1));
