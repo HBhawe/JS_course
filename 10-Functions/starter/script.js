e5afe950ab8043b42a5318979279e50ec4b9be10
Harshal Bhawe <harshalb97@outlook.com>
2024-09-28 19:26:34 +0200

    Closures 2

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -297,18 +297,67 @@
 // CLOSURES
 // super weird concept that technically shouldn't work
 
-const secureBooking = function () {
-  let passengerCount = 0;
+// const secureBooking = function () {
+//   let passengerCount = 0;
 
-  return function () {
-    passengerCount++;
-    console.log(`${passengerCount} passengers`);
+//   return function () {
+//     passengerCount++;
+//     console.log(`${passengerCount} passengers`);
+//   };
+// };
+
+// const booker = secureBooking();
+// booker();
+// booker();
+// booker();
+
+// console.dir(booker);
+
+// CLOSURES 2
+
+// example 1
+
+/*
+let f;
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
   };
 };
 
-const booker = secureBooking();
-booker();
-booker();
-booker();
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
 
-console.dir(booker);
+g();
+f();
+console.dir(f);
+
+// reassigning  f function
+h();
+f();
+console.dir(f);
+
+// example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+// proving that closure has priority.
+// perGroup exists in the closure, so we cannot use the one here:
+const perGroup = 1000;
+boardPassengers(180, 3);
+*/
+
+// CODING CHALLENGE 2
