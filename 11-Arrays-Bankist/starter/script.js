f1e0fe4b4de136620abc03b286748b5113afe121
HBhawe <harshalb97@outlook.com>
2024-10-01 11:22:54 +0200

    Arrays challenge 1

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -194,3 +194,21 @@ const currencies = new Map([
 // currenciesUnique.forEach(function (value, _value, map) {
 //   console.log(`${_value}: ${value}`);
 // });
+
+const checkDogs = function (dogsJulia, dogsKate) {
+  const dogsJuliaCorrected = dogsJulia.slice();
+  dogsJuliaCorrected.splice(0, 1);
+  dogsJuliaCorrected.splice(-2);
+  // console.log(dogsJuliaCorrected);
+
+  const dogsBoth = dogsJuliaCorrected.concat(dogsKate);
+  // console.log(dogsBoth);
+  dogsBoth.forEach(function (age, i) {
+    if (age >= 3) {
+      console.log(`Dog number ${i + 1} is an adult, and is ${age} years old`);
+    } else console.log(`Dog number ${i + 1} is still a puppy`);
+  });
+};
+
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
