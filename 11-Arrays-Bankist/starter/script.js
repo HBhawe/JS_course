539629e604c4062bb701baf1f023bae49fbcbf69
HBhawe <harshalb97@outlook.com>
2024-10-01 15:30:00 +0200

    Arrays coding challenge 3

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -363,22 +363,20 @@ console.log(minValue);
 
 // CODING CHALLENGE 2 and 3
 // I initially made 3 separate methods but for convenience I chained them all together
-/*
-const calcAverageHumanAge = function (ages) {
-  const humanAge = ages
+
+const calcAverageHumanAge = (ages) =>
+  ages
     .map((age) => (age > 2 ? 16 + age * 4 : 2 * age))
     .filter((age) => age >= 18)
     .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
-  return humanAge;
-};
 
 console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
 console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
-*/
 
 // we can keep chaining as long as the previous method returns an array
 // we cannot chain anything after reduce as it returns a single value
 // the current "arr" value in the callback function can be used to check the value during the operation
+/*
 const totalDepositsUSD = movements
   .filter((mov) => mov > 0)
   // .map((mov) => mov * eurToUsd)
@@ -388,3 +386,4 @@ const totalDepositsUSD = movements
   })
   .reduce((acc, mov) => acc + mov);
 console.log(totalDepositsUSD);
+*/
