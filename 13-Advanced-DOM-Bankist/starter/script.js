e2c8193f3ecb3f4c631047f5a1cbf0692a0f8c37
HBhawe <harshalb97@outlook.com>
2024-10-04 10:59:09 +0200

    Advanced DOM 2

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -30,3 +30,53 @@ document.addEventListener("keydown", function (e) {
     closeModal();
   }
 });
+
+// SELECTING ELEMENTS
+/*
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+console.log(document.querySelector(".header"));
+
+const allSections = document.querySelectorAll("section");
+console.log(allSections);
+
+console.log(document.getElementById("section--1"));
+
+// returns an HTML collection - this is a "LIVE" selector - updates automatically based on content
+const allButtons = document.getElementsByTagName("button");
+console.log(allButtons);
+
+console.log(document.getElementsByClassName("btn"));
+*/
+
+// CREATING AND INSERTING ELEMENTS
+
+// .insertAdjacentHTML
+
+const header = document.querySelector(".header");
+
+const message = document.createElement("div");
+message.classList.add("cookie-message");
+// message.textContent = "We use cookies for improved functionality";
+message.innerHTML = `We use cookies for improved functionality<button class= "btn btn--close-cookie">Got it!</button>`;
+
+// we can only have one element in once place in the html - this moves the element to the last child
+// header.prepend(message); //first child
+header.append(message); //last child
+// console.log(message);
+
+// cloning node and adding the element
+// header.append(message.cloneNode(true));
+
+// header.before(message);
+// header.after(message);
+
+// DELETE ELEMENT
+// remove the cookie element on clicking the got it button
+document
+  .querySelector(".btn--close-cookie")
+  .addEventListener("click", function () {
+    message.remove();
+  });
