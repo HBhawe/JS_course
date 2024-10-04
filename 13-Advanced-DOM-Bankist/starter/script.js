8efffc7911ebabcb66e905b980eac3ffbe9496d7
HBhawe <harshalb97@outlook.com>
2024-10-04 15:23:42 +0200

    Advanced DOM 3

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -80,3 +80,58 @@ document
   .addEventListener("click", function () {
     message.remove();
   });
+
+// STYLES
+message.style.backgroundColor = "#37383d";
+message.style.width = "120%";
+
+// only works for inline styles that we set
+console.log(message.style.height); //returns empty string
+console.log(message.style.backgroundColor);
+
+// all styles for the element
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+// we need to parseFloat and set the base
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 40 + "px";
+
+// set CSS variables - check :root in the CSS
+document.documentElement.style.setProperty("--color-primary", "orangered");
+
+// ATTRIBUTES of elements
+const logo = document.querySelector(".nav__logo");
+console.log(logo.alt); //alt text
+console.log(logo.src); //src - full path
+console.log(logo.className);
+
+// setting a custom attribute - this won't work - NON STANDARD ATTRIBUTE NAME
+console.log(logo.designer);
+// getting custom attributes
+console.log(logo.getAttribute("designer"));
+
+logo.alt = "Beautiful minimalist logo";
+
+// setting custom attribute
+logo.setAttribute("company", "Bankist");
+
+console.log(logo.getAttribute("src")); //relative path
+
+const link = document.querySelector(".twitter-link");
+console.log(link.href);
+console.log(link.getAttribute("href"));
+
+// DATA attribute
+// data attribute in the html
+// stored in the dataset object
+console.log(logo.dataset.versionNumber);
+
+logo.classList.add("c");
+logo.classList.remove("c");
+logo.classList.toggle("c");
+logo.classList.contains("c");
+
+// don't use - overwrites all existing class
+// logo.className = "jonas";
+// use classList.add instead
