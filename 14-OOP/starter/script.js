8af0476ea85cfb21be4668013b30d36d44921c65
HBhawe <harshalb97@outlook.com>
2024-10-23 15:57:28 +0200

    OOP 12

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -377,6 +377,7 @@ console.log(martha);
 */
 
 // INHERITANCE using object.create
+/*
 const StudentProto = Object.create(personProto);
 
 StudentProto.init = function (firstName, birthYear, course) {
@@ -396,3 +397,51 @@ jay.calcAge();
 
 // jay.init("Jay", 2007);
 // console.log(jay);
+*/
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+    console.log(`Thanks for opening an account ${this.owner}`);
+  }
+
+  // PUBLIC INTERFACE
+  // API
+  deposit(val) {
+    this.movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan appoved`);
+    }
+  }
+}
+
+const acc1 = new Account("Harshal", "INR", 1111);
+console.log(acc1);
+
+// NOT RECOMMENDED
+// acc1.movements.push(250);
+// acc1.movements.push(-140);
+// console.log(acc1);
+
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+acc1.approveLoan(1000);
+
+console.log(acc1);
