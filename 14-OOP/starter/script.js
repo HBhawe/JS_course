6bae474b6bd0d082a22b6c9f1e5f1d043e1ca9b7
HBhawe <harshalb97@outlook.com>
2024-10-23 09:50:45 +0200

    OOP Coding challenge 1

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -13,7 +13,7 @@ const Person = function (firstName, birthYear) {
 };
 
 const harshal = new Person("Harshal", 1997);
-console.log(harshal);
+// console.log(harshal);
 
 // 4 steps with the "new" keyword
 // 1. New {} is created
@@ -39,34 +39,71 @@ harshal.calcAge();
 jack.calcAge();
 matilda.calcAge();
 
-console.log(harshal, jack, matilda);
+// console.log(harshal, jack, matilda);
 
 // prototypal inheritance
 Person.prototype.species = "Homo sapiens sapiens";
 
-console.log(harshal.species, jack.species, matilda.species);
+// console.log(harshal.species, jack.species, matilda.species);
 
-console.log(harshal.hasOwnProperty("firstName"));
-console.log(harshal.hasOwnProperty("species")); //returns false as it gets it through prototypal inheritance
+// console.log(harshal.hasOwnProperty("firstName"));
+// console.log(harshal.hasOwnProperty("species")); //returns false as it gets it through prototypal inheritance
 
-console.log(harshal.__proto__);
-console.log(harshal.__proto__.__proto__);
-console.log(harshal.__proto__.__proto__.__proto__); //  returns null as it Object.prototype is already part of the top level
+// console.log(harshal.__proto__);
+// console.log(harshal.__proto__.__proto__);
+// console.log(harshal.__proto__.__proto__.__proto__); //  returns null as it Object.prototype is already part of the top level
 
-console.dir(Person.prototype.constructor);
+// console.dir(Person.prototype.constructor);
 
 const arr = [3, 56, 7, 2, 56, 7];
-console.log(arr.__proto__);
-console.log(arr.__proto__ === Array.prototype); //  returns true
+// console.log(arr.__proto__);
+// console.log(arr.__proto__ === Array.prototype); //  returns true
 
-console.log(arr.__proto__.__proto__); //  returns object prototype
+// console.log(arr.__proto__.__proto__); //  returns object prototype
 
 // EXTENDING PROTOTYPES IS NOT A GOOD IDEA
 Array.prototype.unique = function () {
   return [...new Set(this)];
 };
 
-console.log(arr.unique());
+// console.log(arr.unique());
 
 const h1 = document.querySelector("h1");
-console.dir((x) => x + 1); //function to object prototype
+// console.dir((x) => x + 1); //function to object prototype
+
+// CODING CHALLENGE 1
+/**
+ * The function `Car` creates a new object representing a car with properties for make and speed.
+ * @param make - Make is a parameter that represents the brand or manufacturer of the car. It could be
+ * a string value such as "Toyota", "Honda", "Ford", etc.
+ * @param speed - The `speed` parameter in the `Car` function represents the speed of the car in km/h. It is
+ * used to store and track the current speed of the car object created using the `Car` constructor
+ * function.
+ */
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(this.speed);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(this.speed);
+};
+
+const bmw = new Car("BMW", 120);
+// console.log(bmw);
+bmw.accelerate();
+bmw.brake();
+// console.log(bmw);
+
+const mercedes = new Car("Mercedes", 95);
+// console.log(mercedes);
+mercedes.accelerate();
+mercedes.brake();
+// console.log(mercedes);
