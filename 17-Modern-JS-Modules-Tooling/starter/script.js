9811e1f61f9b7b1138475598a9a8036cbdc71701
HBhawe <harshalb97@outlook.com>
2024-10-28 15:09:51 +0100

    Tooling 2

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,1 +1,11 @@
-"use strict";
+// importing module
+import {
+  addToCart,
+  totalPrice as price,
+  totalQuantity,
+} from "./shoppingCart.js";
+console.log(`Importing module`);
+
+addToCart(`abcd`, 10);
+
+console.log(price, totalQuantity);
