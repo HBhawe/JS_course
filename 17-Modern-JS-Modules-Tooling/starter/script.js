16e25a23498130449c68d85794d20dbcd38e40d1
HBhawe <harshalb97@outlook.com>
2024-10-28 15:37:55 +0100

    Tooling 3

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -12,5 +12,34 @@
 // ShoppingCart.addToCart("bread", 5);
 
 // default import
-import add from "./shoppingCart.js";
+import add, { cart } from "./shoppingCart.js";
 add("pizza", 2);
+
+// top level await
+// for modules - await works OUTSIDE async functions
+// BLOCKS the entire module
+
+// console.log("start fetching");
+// const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+// const data = await res.json();
+// console.log(data);
+// console.log("something");
+
+const getLastPost = async function () {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+  console.log(data);
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+// returns a promise
+const lastPost = getLastPost();
+console.log(lastPost);
+
+// not very clean
+// lastPost.then((last) => console.log(last));
+
+// using top level await
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
--- 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
+++ 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -1,8 +1,13 @@
 // exporting module
-// console.log(`Exporting module`);
+console.log(`Exporting module`);
+
+// blocking code
+// console.log("start fetching users");
+// await fetch("https://jsonplaceholder.typicode.com/users");
+// console.log("Finish fetching");
 
 const shippingCost = 10;
-const cart = [];
+export const cart = [];
 
 export const addToCart = function (product, quantity) {
   cart.push({ product, quantity });
