be5cee8e1acdede2fec15a1db7ea88bb278887f1
HBhawe <harshalb97@outlook.com>
2024-10-28 11:11:31 +0100

    Promises 7

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -240,3 +240,58 @@ Promise.resolve("Resolved promise 2").then((res) => {
 
 console.log(`test end`);
 */
+
+// PROMISES
+/*
+const lotteryPromise = new Promise(function (resolve, reject) {
+  console.log(`Lottery draw`);
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve(`You WIN`);
+    } else reject(new Error(`You LOSE`));
+  }, 2000);
+});
+
+lotteryPromise
+  .then((res) => console.log(res))
+  .catch((err) => console.error(err));
+
+// promisifying set timeout
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+// consuming the promise
+wait(1)
+  .then(() => {
+    console.log(`1 second passed`);
+    return wait(1);
+  })
+  .then(() => {
+    console.log(`2 seconds passed`);
+    return wait(1);
+  })
+  .then(() => {
+    console.log(`3 seconds passed`);
+    return wait(1);
+  })
+  .then(() => console.log(`4 seconds passed`));
+
+// setTimeout(() => {
+//   console.log('1 second passed');
+//   setTimeout(() => {
+//     console.log('2 seconds passed');
+//     setTimeout(() => {
+//       console.log('3 second passed');
+//       setTimeout(() => {
+//         console.log('4 second passed');
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+
+Promise.resolve(`You WIN abc`).then((x) => console.log(x));
+Promise.reject(new Error(`You LOSE abc`)).catch((x) => console.error(x));
+*/
