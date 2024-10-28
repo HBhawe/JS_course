9f58e98cdece9fdc9fa39520b392ac761878b2c2
HBhawe <harshalb97@outlook.com>
2024-10-28 11:45:10 +0100

    Coding challenge 2

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -349,3 +349,51 @@ btn.addEventListener("click", function () {
   whereAmI();
 });
 */
+
+// CODING CHALLENGE 2
+const images = document.querySelector(".images");
+
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement("img");
+    img.src = imgPath;
+
+    img.addEventListener("load", function () {
+      images.append(img);
+      resolve(img);
+    });
+
+    // reject
+    img.addEventListener("error", function () {
+      reject(new Error(`image not found`));
+    });
+  });
+};
+
+let currentImg;
+
+createImage("img/img-1.jpg")
+  .then((img) => {
+    currentImg = img;
+    console.log(`Image 1 loaded`);
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = "none";
+    return createImage("img/img-2.jpg");
+  })
+  .then((img) => {
+    currentImg = img;
+    console.log(`Image 2 loaded`);
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = "none";
+  })
+  .catch((err) => console.error(err));
