b7ec3c31c83dc33f2f9c19885569e8de33a9012f
HBhawe <harshalb97@outlook.com>
2024-10-28 16:56:58 +0100

    Tooling 3

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -12,6 +12,7 @@
 // ShoppingCart.addToCart("bread", 5);
 
 // default import
+/*
 import add, { cart } from "./shoppingCart.js";
 add("pizza", 2);
 
@@ -43,3 +44,35 @@ console.log(lastPost);
 // using top level await
 const lastPost2 = await getLastPost();
 console.log(lastPost2);
+*/
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to cart`);
+  };
+
+  const orderStock = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart("apple", 4);
+ShoppingCart2.addToCart("pizza", 2);
+console.log(ShoppingCart2);
+
+// cannot access this in the space
+console.log(ShoppingCart2.shippingCost);
