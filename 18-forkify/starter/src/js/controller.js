d721e5c74af256ad3e0937c12d8d098adc9d593d
Harshal Bhawe <harshalb97@outlook.com>
2024-11-01 15:47:02 +0100

    Search result render

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -3,9 +3,14 @@
 import * as model from "./model.js";
 import recipeView from "./views/recipeView.js";
 import searchView from "./views/searchView.js";
+import resultsView from "./views/resultsView.js";
 
 import "core-js/stable";
 import "regenerator-runtime/runtime";
+
+if (module.hot) {
+  module.hot.accept();
+}
 
 const controlRecipes = async function () {
   try {
@@ -31,6 +36,9 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
+    // render spinner
+    resultsView.renderSpinner();
+
     // 1.get search query
     const query = searchView.getQuery();
     if (!query) return;
@@ -39,7 +47,7 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     // 3. render results
-    console.log(model.state.search.results);
+    resultsView.render(model.state.search.results);
   } catch (error) {
     console.log(error);
   }
